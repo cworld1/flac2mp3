bea66187261b7f061e445e23440b89b0908dfe96
CWorld <cworld0@qq.com>
2025-02-19 07:03:55 +0800

    fix: :construction_worker: remove completions

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,13 @@
 set -euo pipefail
 
 export ARTIFACT_NAME="flac2mp3-$1"
-export flac2mp3_GEN_COMPLETIONS=1
 
 # Build for the target
 cargo build --release --locked --target "$1"
 
 # Create the artifact
-mkdir -p "$ARTIFACT_NAME/completions"
+mkdir -p "$ARTIFACT_NAME"
 cp "target/$1/release/flac2mp3" "$ARTIFACT_NAME"
-cp flac2mp3-cli/completions/* "$ARTIFACT_NAME/completions"
-cp flac2mp3-boot/completions/* "$ARTIFACT_NAME/completions"
 cp README.md LICENSE "$ARTIFACT_NAME"
 
 # Zip the artifact
