6a9f87be014f58bfcb797878223bc1896387ed73
CWorld <cworld0@qq.com>
2025-02-19 06:53:17 +0800

    feat: :sparkles: aupport quick build script

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+export ARTIFACT_NAME="flac2mp3-$1"
+export flac2mp3_GEN_COMPLETIONS=1
+
+# Build for the target
+cargo build --release --locked --target "$1"
+
+# Create the artifact
+mkdir -p "$ARTIFACT_NAME/completions"
+cp "target/$1/release/ya" "$ARTIFACT_NAME"
+cp "target/$1/release/flac2mp3" "$ARTIFACT_NAME"
+cp flac2mp3-cli/completions/* "$ARTIFACT_NAME/completions"
+cp flac2mp3-boot/completions/* "$ARTIFACT_NAME/completions"
+cp README.md LICENSE "$ARTIFACT_NAME"
+
+# Zip the artifact
+if ! command -v zip &> /dev/null
+then
+	sudo apt-get update && sudo apt-get install -yq zip
+fi
+zip -r "$ARTIFACT_NAME.zip" "$ARTIFACT_NAME"
